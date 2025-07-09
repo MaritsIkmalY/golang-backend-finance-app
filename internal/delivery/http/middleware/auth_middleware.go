4b8395611a03c44941ff4d78fd4ede1a560b0452
MaritsIkmalY <maritsikmaly@gmail.com>
2025-07-09 18:22:47 +0700

    add: middleware for users

---
+++ internal/delivery/http/middleware/auth_middleware.go
@@ -0,0 +1,43 @@
+package middleware
+
+import (
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware(secret string) fiber.Handler{ 
+	return func(c *fiber.Ctx) error {
+		authHeader := c.Get("Authorization")
+
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Missing or invalid token"})
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return []byte(secret), nil
+		})
+
+		if err != nil || !token.Valid {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid or expired token"})
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok || !token.Valid {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid token claims"})
+		}
+
+		userID, ok := claims["user_id"].(string)
+
+		if !ok {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "user_id not found in token"})
+		}
+
+		c.Locals("user_id", userID)
+
+		return c.Next()
+	}
+}
