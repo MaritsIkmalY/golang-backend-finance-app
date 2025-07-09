574e010f9ff145592437495814405b0b80640921
MaritsIkmalY <maritsikmaly@gmail.com>
2025-07-09 18:23:16 +0700

    update: route auth middleware

--- internal/delivery/http/route/route.go
+++ internal/delivery/http/route/route.go
@@ -1,22 +1,37 @@
 package route
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 
 	"github.com/maritsikmaly/golang-finance-app/internal/delivery/http"
+	"github.com/maritsikmaly/golang-finance-app/internal/delivery/http/middleware"
 )
 
 type RouteConfig struct {
 	App            *fiber.App
 	UserController *http.UserController
+	TransactionController *http.TransactionController
 }
 
 func (c *RouteConfig) SetupRoutes() {
-	auth := c.App.Group("/api/v1/finance-app")
-	guestRoutes(c, auth)
+	api := c.App.Group("/api/v1/finance-app")
+	guestRoutes(c, api)
+
+    auth := api.Group("/", middleware.AuthMiddleware(os.Getenv("JWT_SECRET")))
+
+    authRoutes(c, auth)
 }
 
 func guestRoutes(c *RouteConfig, auth fiber.Router) {
 	auth.Post("/register", c.UserController.Register)
 	auth.Post("/login", c.UserController.Login)
+}
+
+func authRoutes(c *RouteConfig, auth fiber.Router) {
+	auth.Post("/transactions", c.TransactionController.Create)
+	auth.Get("/transactions", c.TransactionController.GetByUserID)
+	auth.Put("/transactions/:id", c.TransactionController.Update)
+	auth.Delete("/transactions/:id", c.TransactionController.Delete)
 }
